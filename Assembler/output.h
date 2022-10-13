bb4aa26afa293d3cebbb2f6e79f1021269ebe997
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-10-13 15:42:34 +0300

    Update output.h

--- Assembler/output.h
+++ Assembler/output.h
@@ -7,20 +7,20 @@
 *into ".ob" type file. if both tables are empty, will create an empty ".ob" type file.
 *@param startOfFile - a string which contains the name of a file before the '.'
 */
-void createObjectOutputFile(char * startOfFile);
+void createObjectOutputFile(char* startOfFile);
 
 /**
 *The function gets a name of a file, checks if the Labels Table contains one or more entry type labels and prints them (by order) 
 *into ".ent" type file. if there is no entry type labels at all, will not create the ".ent" file.
 *@param startOfFile - a string which contains the name of a file before the '.'
 */
-void createEntryOutputFile(char * startOfFile);
+void createEntryOutputFile(char* startOfFile);
 
 /**
 *The function gets a name of a file, checks if the Externals Table contains at least one line and prints them (by order) 
 *into ".ext" type file. if there is no lines at all, will not create the ".ext" file.
 *@param startOfFile - a string which contains the name of a file before the '.'
 */
-void createExternalOutputFile(char * startOfFile);
+void createExternalOutputFile(char* startOfFile);
 
 #endif
