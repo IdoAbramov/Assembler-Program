0ae8f5b1a17eca8d84cc626d04d7505c5ca1d83b
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-10-13 15:48:38 +0300

    Update utils.h

--- Assembler/utils.h
+++ Assembler/utils.h
@@ -42,9 +42,19 @@
 #define MIN_NUMBER_SIZE -2048 /*the minumum number which can be represented as 12 bits signed number (2^11)*/
 
 /* enumerations */
-typedef enum {FALSE = 0,TRUE = 1} boolean; /*used for boolean variable definition*/
-typedef enum {AS = 1,OB = 2,ENT = 3,EXT = 4} fileType;/*used for files types*/
-typedef enum {NO_ADDRESS_MODE = -1, IMMEDIATE = 0, DIRECT = 1, RELATIVE = 2, REGISTER_DIRECT = 3} addressingMode;/*used for addressing mode types*/
+typedef enum {FALSE = 0,
+              TRUE = 1} boolean; /*used for boolean variable definition*/
+
+typedef enum {AS = 1,
+              OB = 2,
+              ENT = 3,
+              EXT = 4} fileType;/*used for files types*/
+
+typedef enum {NO_ADDRESS_MODE = -1,
+              IMMEDIATE = 0,
+              DIRECT = 1,
+              RELATIVE = 2,
+              REGISTER_DIRECT = 3} addressingMode;/*used for addressing mode types*/
 
 
 /* extern global variables to use in the program */
@@ -80,7 +90,7 @@ void memoryAllocationFailure();
 *@param line - the full line string ends with '\0'
 *@param word - the full word string which will contain the next word from the line
 */
-void getNextWordInLine(char * line, char * word);
+void getNextWordInLine(char* line, char* word);
 
 /**
 *The function gets a start of file name and its type and returns the full file name with its matching end of file name
@@ -88,7 +98,7 @@ void getNextWordInLine(char * line, char * word);
 *@param fileType - an enumerated value which represents the file requested end of file
 *@return a string of the full file name 
 */
-char * getFileName(char * startOfFile, int fileType);
+char* getFileName(char* startOfFile, int fileType);
 
 
 #endif
