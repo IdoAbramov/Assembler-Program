68f2e5b372dda1ac38de9f5d54eba8cfeebd883b
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-10-13 15:43:22 +0300

    Update validators.h

--- Assembler/validators.h
+++ Assembler/validators.h
@@ -6,69 +6,69 @@
 *@param line - contains the full command line to check
 *@return TRUE/FALSE
 */
-int isEmptyOrCommentLine(const char * line);
+int isEmptyOrCommentLine(const char* line);
 
 /**
 *The function checks if the input string is considered as saved word.
 *@param str - string to check 
 *@return TRUE/FALSE
 */
-int isSavedWord(const char * str);
+int isSavedWord(const char* str);
 
 /**
 *The function checks if the input string is considered as number.
 *@param strNum - string to check
 *@return TRUE/FALSE
 */
-int isLegalNumber(const char * strNum);
+int isLegalNumber(const char* strNum);
 
 /**
 *The function checks if the input string is considered as legal label (starts with alphabetic letter and ends with ':')
 *@param str - string to check
 *@return TRUE/FALSE
 */
-int isLegalLabel(const char * str);
+int isLegalLabel(const char* str);
 
 /**
 *The function checks if the input string is considered as legal operation code.
 *@param str - string to check
 *@return TRUE/FALSE
 */
-int isOpcode(const char * str);
+int isOpcode(const char* str);
 
 /**
 *The function checks if the input string is considered as legal register
 *@param str - string to check
 *@return TRUE/FALSE
 */
-int isRegister(const char * str);
+int isRegister(const char* str);
 
 /**
 *The function checks if the input string is considered as legal directive
 *@param str - string to check
 *@return TRUE/FALSE
 */
-int isDirective(const char * str);
+int isDirective(const char* str);
 
 /**
 *The function checks if the input string is a label which is already inside the Labels Table
 *@param str - string to check
 *@return TRUE/FALSE
 */
-int isLabelAlreadyInTable(const char * str);
+int isLabelAlreadyInTable(const char* str);
 
 /**
 *The function checks if the input string is a legal operand number (starts with '#') - which is a number used with operation codes
 *@param str - string to check
 *@return TRUE/FALSE
 */
-int isOperandNumber(char * strNum);
+int isOperandNumber(char* strNum);
 
 /**
 *The function checks if the input string is a legal relative address (starts with '%')
 *@param label - string to check
 *@return TRUE/FALSE
 */
-int isRelativeAddress(char * label);
+int isRelativeAddress(char* label);
 
 #endif
