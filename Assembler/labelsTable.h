77f7461ca388dd130e34a6aa355667d7852e9fc5
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-10-13 15:40:00 +0300

    Update labelsTable.h

--- Assembler/labelsTable.h
+++ Assembler/labelsTable.h
@@ -2,15 +2,20 @@
 #define LABELS_TABLE_H
 
 /*define new variable type of label type*/
-typedef enum {NO_TYPE = 0,DATA = 1,CODE = 2,EXTERN = 3,CODE_ENTRY = 4, DATA_ENTRY =5} labelType;
+typedef enum {NO_TYPE = 0,
+	      DATA = 1,
+	      CODE = 2,
+	      EXTERN = 3,
+	      CODE_ENTRY = 4,
+	      DATA_ENTRY =5} labelType;
 
 /*structure of label node contains its name, address and its label type.
 also has pointer for the next label node in the Labels Table*/
 typedef struct labelNode{
-	char * name;
+	char* name;
 	int address;
 	labelType type; 
-	struct labelNode * next;
+	struct labelNode* next;
 }labelNode;
 
 #endif
