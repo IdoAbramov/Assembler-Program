b64c8d7fb1a2ed51f835997a43aeeb783d0a9b4d
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-10-25 11:05:45 +0300

    Update errorsTable.h

--- Assembler/errorsTable.h
+++ Assembler/errorsTable.h
@@ -3,11 +3,11 @@
 
 /*structure of error node contains its description, the line and the file which the error had occurred.
 also has pointer for the next error node in the Errors Table*/
-typedef struct errorNode{
+typedef struct errorNode {
 	char* description;
 	int line;
 	char* file;
 	struct errorNode* next;
-}errorNode;
+} errorNode;
 
 #endif
